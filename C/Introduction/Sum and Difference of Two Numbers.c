f37adba9da1cf345b6807c418fca549b77100ba0
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2022-09-11 23:46:17 +0530

    Sum and Difference of Two Numbers
    Problem link to try it yourself : https://www.hackerrank.com/challenges/sum-numbers-c/problem?isFullScreen=true

---
+++ C/Introduction/Sum and Difference of Two Numbers.c
@@ -0,0 +1,39 @@
+/*
+Task
+
+Your task is to take two numbers of int data type, two numbers of float data type as input and output their sum:
+
+Declare  variables: two of type int and two of type float.
+Read  lines of input from stdin (according to the sequence given in the 'Input Format' section below) and initialize your  variables.
+Use the  and  operator to perform the following operations:
+Print the sum and difference of two int variable on a new line.
+Print the sum and difference of two float variable rounded to one decimal place on a new line.
+Input Format
+
+The first line contains two integers.
+The second line contains two floating point numbers.
+
+Constraints
+
+ integer variables 
+ float variables 
+Output Format
+
+Print the sum and difference of both integers separated by a space on the first line, and the sum and difference of both float (scaled to  decimal place) separated by a space on the second line.
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a,b;
+    float c,d;
+    scanf("%d %d %f %f",&a,&b,&c,&d);
+    printf("%d %d\n",a+b, a-b);
+    printf("%.1f %.1f",c+d,c-d);
+	
+    return 0;
+}
