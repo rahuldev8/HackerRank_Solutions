a2a353645c3307116a5f4a564148cc7739bb400f
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2022-09-11 23:58:14 +0530

    Ruby Tutorial - Object Methods
    Problem link to try it yourself : https://www.hackerrank.com/challenges/ruby-tutorial-object-methods/problem?isFullScreen=true

---
+++ Ruby/Introduction/Ruby Tutorial - Object Methods.rb
@@ -0,0 +1,28 @@
+=begin
+
+Each object in Ruby may have methods associated with it. To demonstrate this, we want you to print whether a number is even or odd. A number has an even? method associated with it, which returns true or false based on the parity of the number.
+
+Assuming a variable number is already defined, check whether a given number is even or not.
+
+Hint
+
+Type in the code-editor
+
+return number.even?
+or
+
+number.even?
+Input Format
+
+The first line of input contains an integer . The next  contains one integer in each line.
+
+Output Format
+
+The output is handled by the code written in the editor.
+=end
+
+
+
+    return number.even?
+    # add your code here
+
