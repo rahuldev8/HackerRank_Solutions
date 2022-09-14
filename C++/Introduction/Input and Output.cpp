187df2f06bf7d64aec1e4826482ef90e3bdedb7e
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2022-09-14 23:06:41 +0530

    Input and Output
    Problem link to try it yourself : https://www.hackerrank.com/challenges/cpp-input-and-output/problem?isFullScreen=true

---
+++ C++/Introduction/Input and Output.cpp
@@ -0,0 +1,30 @@
+/*
+Task
+Read  numbers from stdin and print their sum to stdout.
+
+Input Format
+
+One line that contains  space-separated integers: , , and .
+
+Constraints
+
+Output Format
+
+Print the sum of the three numbers on a single line.*/
+
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    int a,b,c;
+    cin>>a>>b>>c;
+    cout<<a+b+c; 
+    return 0;
+}
