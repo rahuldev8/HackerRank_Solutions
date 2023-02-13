9ae79f4c731b2d764176d9a0dc7f74eeee1b0f4c
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2023-02-13 21:37:47 +0530

    Java Method Overriding 2 (Super Keyword)
    Problem link to try it Yourself : https://www.hackerrank.com/challenges/java-method-overriding-2-super-keyword/problem?isFullScreen=true

---
+++ Java/Object Oriented Programming/Java Method Overriding 2 (Super Keyword).java
@@ -0,0 +1,37 @@
+/*
+When a method in a subclass overrides a method in superclass, it is still possible to call the overridden method using super keyword. If you write super.func() to call the function func(), it will call the method that was defined in the superclass.
+
+You are given a partially completed code in the editor. Modify the code so that the code prints the following text:
+
+Hello I am a motorcycle, I am a cycle with an engine.
+My ancestor is a cycle who is a vehicle with pedals.
+*/
+
+import java.util.*;
+import java.io.*;
+
+class BiCycle{
+	String define_me(){
+		return "a vehicle with pedals.";
+	}
+}
+
+class MotorCycle extends BiCycle{
+	String define_me(){
+		return "a cycle with an engine.";
+	}
+	
+	MotorCycle(){
+		System.out.println("Hello I am a motorcycle, I am "+ define_me());
+
+		String temp=super.define_me(); //Fix this line
+
+		System.out.println("My ancestor is a cycle who is "+ temp );
+	}
+	
+}
+class Solution{
+	public static void main(String []args){
+		MotorCycle M=new MotorCycle();
+	}
+}
