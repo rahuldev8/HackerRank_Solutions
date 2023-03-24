5f016bb6ff77479efe35e37ce91deea1b1c6a556
Rahul Sundararaj <109261169+Dev-jr-8@users.noreply.github.com>
2023-03-24 18:42:13 +0530

    Weather Observation Station 6
    Problem link to try it Yourself : https://www.hackerrank.com/challenges/weather-observation-station-6/problem?isFullScreen=true

---
+++ SQL/Weather Observation Station 6.sql
@@ -0,0 +1,13 @@
+/*
+Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
+
+Input Format
+
+The STATION table is described as follows:
+
+Station.jpg
+
+where LAT_N is the northern latitude and LONG_W is the western longitude.
+*/
+
+SELECT DISTINCT City FROM STATION WHERE City REGEXP '^[aeiouAEIOU]'
